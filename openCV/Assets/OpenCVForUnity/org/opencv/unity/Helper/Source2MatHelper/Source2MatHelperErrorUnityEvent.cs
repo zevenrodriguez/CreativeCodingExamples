134719759d0d39f133daa1ea3721231574350bf2
Zevensuy Rodriguez <zevenrodriguez@users.noreply.github.com>
2026-08-18 20:36:06 -0400

    removed old code

--- openCV/Assets/OpenCVForUnity/org/opencv/unity/Helper/Source2MatHelper/Source2MatHelperErrorUnityEvent.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using UnityEngine.Events;
-
-namespace OpenCVForUnity.UnityIntegration.Helper.Source2Mat
-{
-    [Serializable]
-    public class Source2MatHelperErrorUnityEvent : UnityEvent<Source2MatHelperErrorCode, string> { }
-}
